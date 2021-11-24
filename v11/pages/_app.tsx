82755734e3b288863edc23f9c9c1ea662bd8a245
Nuhutuh25 <73873324+Bigandy4climate@users.noreply.github.com>
2021-11-24 16:39:40 +0100

    v12

---
+++ v11/pages/_app.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import ScrollToTop from "../helpers/ScrollToTop";
+
+
+function App() {
+
+  return (
+
+    <ScrollToTop>
+
+    </ScrollToTop>
+
+  );
+}
+
+export default App;
---
+++ v11/pages/index.tsx
@@ -0,0 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./assets/css/style.css";
+import App from "./_app";
+ReactDOM.render(<App />, document.getElementById("main"));
