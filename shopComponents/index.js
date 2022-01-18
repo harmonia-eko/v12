51afc3e0e91461087a42d523d5a9e2854439527f
Samuel Nihoul <snolly@protonmail.com>
2022-01-18 21:21:34 +0100

    put all files where they belong

--- shopComponents/index.js
+++ shopComponents/index.js
@@ -1,5 +1,5 @@
 export { default as Navbar } from './Navbar/Navbar';
 export { default as Products } from './Products/Products';
-export { default as Cart } from '../../Cart';
+export { default as Cart } from '../../cart';
 export { default as Checkout } from './CheckoutForm/Checkout/Checkout';
 
