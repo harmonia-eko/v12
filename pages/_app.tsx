6b139b436a6176a672166c000382f8da0d86c5a7
Samuel Nihoul <snolly@protonmail.com>
2022-01-18 21:21:31 +0100

    put all files where they belong

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,88 @@ import React from "react";
 import '../css/style.css'
 import "bootstrap/dist/css/bootstrap.min.css";
 import '../public/icofont/icofont.min.css'
+import React, { useState, useEffect } from 'react';
+import { CssBaseline } from '@material-ui/core';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import {useRouter} from 'Next/router'
+import { Navbar, Products, Cart, Checkout } from '../shopComponents';
+import { commerce } from './lib/commerce';
+
+const App = () => {
+  const [mobileOpen, setMobileOpen] = React.useState(false);
+  const [products, setProducts] = useState([]);
+  const [cart, setCart] = useState({});
+  const [order, setOrder] = useState({});
+  const [errorMessage, setErrorMessage] = useState('');
+
+  const fetchProducts = async () => {
+    const { data } = await commerce.products.list();
+
+    setProducts(data);
+  };
+
+  const fetchCart = async () => {
+    setCart(await commerce.cart.retrieve());
+  };
+
+  const handleAddToCart = async (productId, quantity) => {
+    const item = await commerce.cart.add(productId, quantity);
+
+    setCart(item.cart);
+  };
+
+  const handleUpdateCartQty = async (lineItemId, quantity) => {
+    const response = await commerce.cart.update(lineItemId, { quantity });
+
+    setCart(response.cart);
+  };
+
+  const handleRemoveFromCart = async (lineItemId) => {
+    const response = await commerce.cart.remove(lineItemId);
+
+    setCart(response.cart);
+  };
+
+  const handleEmptyCart = async () => {
+    const response = await commerce.cart.empty();
+
+    setCart(response.cart);
+  };
+
+  const refreshCart = async () => {
+    const newCart = await commerce.cart.refresh();
+
+    setCart(newCart);
+  };
+
+  const handleCaptureCheckout = async (checkoutTokenId, newOrder) => {
+    try {
+      const incomingOrder = await commerce.checkout.capture(checkoutTokenId, newOrder);
+
+      setOrder(incomingOrder);
+
+      refreshCart();
+    } catch (error) {
+      setErrorMessage(error.data.error.message);
+    }
+  };
+
+  useEffect(() => {
+    fetchProducts();
+    fetchCart();
+  }, []);
+
+  const handleDrawerToggle = () => setMobileOpen(!mobileOpen);
+
+/* switch (useRouter.pathname) {
+  case ('/shop'):return <Products products={products} onAddToCart={handleAddToCart} handleUpdateCartQty />
+  case ('/cart'): return<Cart cart={cart} onUpdateCartQty={handleUpdateCartQty} onRemoveFromCart={handleRemoveFromCart} onEmptyCart={handleEmptyCart} />
+  case ('/checkout'):    return    <Checkout cart={cart} order={order} onCaptureCheckout={handleCaptureCheckout} error={errorMessage} />
+}
+ */
+  
+};
+
 
 export default function App({ Component, pageProps }) {
   return <Component className="App" {...pageProps} />
