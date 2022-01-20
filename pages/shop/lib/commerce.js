c1109f6024b9a4d343e80e780e0fa00f431d7950
Samuel Nihoul <snolly@protonmail.com>
2022-01-20 15:54:56 +0100

    everything fine except image and icon

--- pages/shop/lib/commerce.js
+++ pages/shop/lib/commerce.js
@@ -1,3 +1,5 @@
 import Commerce from '@chec/commerce.js';
+export const commerce = new Commerce(process.env.NEXT_PUBLIC_APP_CHEC_PUBLIC_KEY, true);
 
-export const commerce = new Commerce(process.env.REACT_APP_CHEC_PUBLIC_KEY, true);
+
+
